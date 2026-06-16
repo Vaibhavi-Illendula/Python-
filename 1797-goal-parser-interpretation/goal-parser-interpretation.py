1ed553aed6c96dc809765eab87f576198bfa99fa
Illendula Vaibhavi <illendulavaibhavi@gmail.com>
2026-06-16 21:50:21 +0530

    Time: 53 ms (6.92%) | Memory: 19.2 MB (46.46%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        s = ""
+        i = 0
+        while i < len(command):
+            if command[i] == 'G':
+                s += 'G'
+                i += 1
+            elif command[i:i+2] == "()":
+                s += 'o'
+                i += 2
+            else:  
+                s += 'al'
+                i += 4
+
+        return s
