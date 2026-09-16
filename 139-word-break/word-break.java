6810a648f912489e034f9053230755d1249fdac3
Illendula Vaibhavi <illendulavaibhavi@gmail.com>
2026-09-16 19:47:23 +0530

    Time: 8 ms (48.89%) | Memory: 46.2 MB (34.63%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,26 +1,49 @@
+// class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+
+//         boolean[] dp = new boolean[s.length() + 1];
+
+//         dp[0] = true;
+
+//         for (int i = 1; i <= s.length(); i++) {
+
+//             for (String word : wordDict) {
+
+//                 int len = word.length();
+
+//                 if (i >= len) {
+
+//                     if (dp[i - len] && s.substring(i - len, i).equals(word)) {
+//                         dp[i] = true;
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return dp[s.length()];
+//     }
+// }
+
+
+import java.util.*;
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-
-        boolean[] dp = new boolean[s.length() + 1];
-
-        dp[0] = true;
-
-        for (int i = 1; i <= s.length(); i++) {
-
-            for (String word : wordDict) {
-
-                int len = word.length();
-
-                if (i >= len) {
-
-                    if (dp[i - len] && s.substring(i - len, i).equals(word)) {
-                        dp[i] = true;
-                        break;
-                    }
-                }
+     Set<String> set = new HashSet<>(wordDict);
+     boolean[] d = new boolean[s.length()+1];
+     d[0]=true;
+     for(int i=1;i<=s.length();i++)
+     {
+        for(int j=0;j<i;j++)
+        {
+            if(d[j] && set.contains(s.substring(j,i)))
+            {
+                d[i]=true;
+                break;
             }
         }
+     }
 
-        return dp[s.length()];
+     return d[s.length()];
     }
 }
